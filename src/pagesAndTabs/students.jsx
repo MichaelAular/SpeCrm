fead7679c651e232ae6f0be0a367c6cae800b59e
Michael Aular <michaelaular@gmail.com>
2024-02-07 15:50:48 +0100

    student page mediaQ

--- src/pagesAndTabs/students.jsx
+++ src/pagesAndTabs/students.jsx
@@ -3,25 +3,24 @@ import styles from "../app/page.module.scss";
 export function Page_Students({ profiles, setProfileID, setCurrentPage, setCurrentTab }) {
 
   return (
-    <div>
-      <div className={styles.textContainer} />
+    <main className={styles.mainCentered} style={{paddingTop: "6rem"}}>
+        <h1 className="pageTitle" style={{marginBottom: "12px"}}>Studenten</h1>
+        <div style={{ display: "flex", flexDirection: "column" }}>
 
-      <main className={styles.main}>
-      <h1 className="pageTitle" style={{marginBottom: "12px"}}>Studenten</h1>
-        {profiles && profiles.list.map((student) =>
-          <button
-            className="studentBtn"
-            key={student.id}
-            onClick={()=>{
-              setProfileID(student.id)
-              setCurrentPage("Analyse")
-              setCurrentTab("Profielschets")
-            }}
-          >
-            {student.firstName} {student.lastName}
-          </button>
-          )}
+          {profiles && profiles.list.map((student) =>
+            <button
+              className="studentBtn"
+              key={student.id}
+              onClick={()=>{
+                setProfileID(student.id)
+                setCurrentPage("Analyse")
+                setCurrentTab("Profielschets")
+              }}
+            >
+              {student.firstName} {student.lastName}
+            </button>
+            )}
+        </div>
       </main>
-    </div>
   );
 }
