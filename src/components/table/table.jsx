52c2744b0b72e947c776fdba1441e0f8db4f1ce2
Michael Aular <michaelaular@gmail.com>
2024-03-28 15:42:43 +0100

    studentpage row per page

--- src/components/table/table.jsx
+++ src/components/table/table.jsx
@@ -1,13 +1,88 @@
 import "./table.scss";
 import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
 import Table from "@mui/material/Table";
 import TableBody from "@mui/material/TableBody";
 import TableCell from "@mui/material/TableCell";
 import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
+import TableFooter from "@mui/material/TableFooter";
 import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
+import TableHead from "@mui/material/TableHead";
+import mockup from "../../../mockup.json";
+
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton
+        onClick={handleBackButtonClick}
+        disabled={page === 0}
+        aria-label="previous page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowRight />
+        ) : (
+          <KeyboardArrowLeft />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? (
+          <KeyboardArrowLeft />
+        ) : (
+          <KeyboardArrowRight />
+        )}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
 
 export function BasicTable({
   profiles,
@@ -15,38 +90,23 @@ export function BasicTable({
   setCurrentTab,
   setProfileID,
 }) {
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
   const [page, setPage] = React.useState(0);
-  4;
-  const [order, setOrder] = React.useState("asc");
-  const [orderBy, setOrderBy] = React.useState("calories");
-
-  const handleRequestSort = (event, property) => {
-    const isAsc = orderBy === property && order === "asc";
-    setOrder(isAsc ? "desc" : "asc");
-    setOrderBy(property);
-  };
-
+  const [rowsPerPage, setRowsPerPage] = React.useState(10);
+  const emptyRows =
+    page > 0 ? Math.max(0, (1 + page) * rowsPerPage - rows.length) : 0;
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
   };
-
   const handleChangeRowsPerPage = (event) => {
     setRowsPerPage(parseInt(event.target.value, 10));
     setPage(0);
   };
-
-  const handleChangeDense = (event) => {
-    setDense(event.target.checked);
-  };
-
-  const isSelected = (id) => selected.indexOf(id) !== -1;
-
   const createData = (bsn, firstName, lastName) => {
     return { bsn, firstName, lastName };
   };
   const rows = () => {
     let rowArray = [];
+    // mockup.map((student) => {
     profiles.list.map((student) => {
       rowArray.push(
         createData(student.id, student.firstName, student.lastName)
@@ -54,6 +114,7 @@ export function BasicTable({
     });
     return rowArray;
   };
+
   const TR = (row) => {
     const [hovered, setHovered] = useState(false);
     return (
@@ -79,36 +140,71 @@ export function BasicTable({
   };
 
   return (
-    <>
-      <TableContainer component={Paper} className="prevent-select">
-        <Table
-          sx={{
-            width: "Calc(100vw - 20px)",
-            maxWidth: "800px",
-          }}
-          aria-label="simple table"
-        >
-          <TableHead>
+    <TableContainer component={Paper}>
+      <Table
+        sx={{
+          width: "Calc(100vw - 4rem)",
+          maxWidth: "800px",
+        }}
+        aria-label="custom pagination table"
+      >
+        <TableHead>
             <TableRow>
-              <TableCell align="left">BSN</TableCell>
-              <TableCell align="right">Voornaam</TableCell>
-              <TableCell align="right">Achternaam</TableCell>
+              <TableCell>BSN</TableCell>
+              <TableCell>Voornaam</TableCell>
+              <TableCell>Achternaam</TableCell>
             </TableRow>
           </TableHead>
+        <TableBody>
+          {(rowsPerPage > 0
+            ? rows().slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+            : rows
+          ).map((row) => (
+            <TableRow
+              key={row.bsn}
+              sx={{ cursor: "pointer" }}
+              onClick={() => {
+                setProfileID(row.bsn);
+                setCurrentPage("Analyse");
+                setCurrentTab("Profielschets");
+              }}
+            >
+              <TableCell >{row.bsn}</TableCell>
+              <TableCell >{row.firstName}</TableCell>
+              <TableCell >{row.lastName}</TableCell>
+            </TableRow>
+          ))}
 
-          <TableBody>{rows().map((row) => TR(row))}</TableBody>
-        </Table>
-      </TableContainer>
-
-      <TablePagination
-        rowsPerPageOptions={[10, 25, 100]}
-        component="div"
-        count={rows.length}
-        rowsPerPage={rowsPerPage}
-        page={page}
-        onPageChange={handleChangePage}
-        onRowsPerPageChange={handleChangeRowsPerPage}
-      />
-    </>
+          {/* {rows().map((row) => TR(row))} */}
+          {emptyRows > 0 && (
+            <TableRow style={{ height: 53 * emptyRows }}>
+              <TableCell colSpan={6} />
+            </TableRow>
+          )}
+        </TableBody>
+        <TableFooter>
+          <TableRow>
+            <TablePagination
+              rowsPerPageOptions={[10, 25, 100]}
+              colSpan={3}
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              slotProps={{
+                select: {
+                  inputProps: {
+                    "aria-label": "rows per page",
+                  },
+                  native: true,
+                },
+              }}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+              ActionsComponent={TablePaginationActions}
+            />
+          </TableRow>
+        </TableFooter>
+      </Table>
+    </TableContainer>
   );
 }
