30f8cdcc73088e6e790511b84eb32132d1a8ab54
Michael Aular <michaelaular@gmail.com>
2024-04-11 12:36:34 +0200

    styled calendar

--- src/components/weekPicker/weekpicker.jsx
+++ src/components/weekPicker/weekpicker.jsx
@@ -6,6 +6,8 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import { DateCalendar } from "@mui/x-date-pickers/DateCalendar";
 import { PickersDay } from "@mui/x-date-pickers/PickersDay";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 
 const CustomPickersDay = styled(PickersDay, {
   shouldForwardProp: (prop) => prop !== "isSelected" && prop !== "isHovered",
@@ -67,12 +69,24 @@ export function WeekPicker({value, setValue}) {
     backgroundColor: "rgb(var(--white04))",
     borderRadius: "5px",
     height: !hoveredElement ? "30px" : "370px",
-    width: "340px",
+    width: "350px",
     paddingTop: "0px",
-    transitionDuration: ".3s",
+    transitionDuration: ".1s",
     boxShadow: "-5px 5px 7px rgba(var(--TextOnWhite), 0.15)",
     overflow: "hidden"
     }
+
+  const weekButton =( type )=> {
+    return (
+      <button className="currentDateButton" onClick={()=>{
+        const newWeek = type === "next" ? value.add(7, 'day') : value.subtract(7, 'day') ;
+        setValue(newWeek)
+      }}>
+        {type === "prev" && <KeyboardArrowLeft/>}
+        {type === "next"&& <KeyboardArrowRight/>}
+        </button>
+    )
+  }
 
   return (
     <dir
@@ -81,7 +95,11 @@ export function WeekPicker({value, setValue}) {
         onMouseLeave={()=>{setHoveredElement(false)}}
         style={style}
     >
-       <span className="currentDate">Week {dayjs(value).week()} {value.$y}</span>
+       <span className="currentDate">
+        {weekButton("prev")}
+        <div className="currentDateText">Week {dayjs(value).week()} {value.$y}</div>
+        {weekButton("next")}
+        </span>
     <LocalizationProvider
         dateAdapter={AdapterDayjs}
         sx={{
